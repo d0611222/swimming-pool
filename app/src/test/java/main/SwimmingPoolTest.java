94e698e1aef64ab224517b3cdaa5e4922c1dfec9
d0611222 <d0611222@mail.fcu.edu.tw>
2021-06-02 20:49:48 +0800

    version 1

--- app/src/test/java/main/SwimmingPoolTest.java
+++ app/src/test/java/main/SwimmingPoolTest.java
@@ -20,36 +20,5 @@ class SwimmingPoolTest {
 	void testGetPrice04() {
 		assertEquals(200*0.5, test.getPrice(false, 21, 8, false, true));
 	}
-	@Test
-	void testGetPrice05() {
-		assertEquals(200*0.8, test.getPrice(false, 12, 8, false, false));
-	}
-	@Test
-	void testGetPrice06() {
-		assertEquals(200*0.8, test.getPrice(false, 60, 8, false, false));
-	}
-	@Test
-	void testGetPrice07() {
-		assertEquals(200*0.8, test.getPrice(false, 21, 6, false, false));
-	}
-	@Test
-	void testGetPrice08() {
-		assertEquals(-1, test.getPrice(false, 1, 8, false, false));
-	}
-	@Test
-	void testGetPrice09() {
-		assertEquals(-1, test.getPrice(false, 80, 8, false, false));
-	}
-	@Test
-	void testGetPrice10() {
-		assertEquals(-1, test.getPrice(false, 21, 3, false, false));
-	}
-	@Test
-	void testGetPrice11() {
-		assertEquals(-1, test.getPrice(false, 21, 24, false, false));
-	}
-	@Test
-	void testGetPrice12() {
-		assertEquals(200*0.7, test.getPrice(false, 21, 8, true, false));
-	}
+
 }
