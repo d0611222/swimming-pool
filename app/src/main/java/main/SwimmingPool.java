eb5301e09b1bdf848c02d0aee78cc7ac5b8861ee
d0611222 <d0611222@mail.fcu.edu.tw>
2021-06-02 20:36:48 +0800

    my swimming pool

---
+++ app/src/main/java/main/SwimmingPool.java
@@ -0,0 +1,27 @@
+
+public class SwimmingPool {
+	public int getPrice(boolean isWeekend, int age, int time, boolean isGroup, boolean isMember) {
+		int price = 200;
+		if(time<5 || 22<time || age<3 || 75<age) {
+			price = -1;
+		}
+		else {
+			if(isWeekend) {
+				price = 250;
+				if(isMember)
+					price *= 0.5; 
+			}
+			else {
+				if(isMember)
+					price *= 0.5;
+				else if(isGroup)
+					price *= 0.7;
+				else if(time < 7)
+					price *= 0.8;
+				else if(age <=12 || 60 <= age)
+					price *= 0.8;
+			}
+		}						
+		return price;
+	}
+}
