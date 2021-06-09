d84f15f639875f887f88efbe1eecf70a4978a743
d0611222 <d0611222@mail.fcu.edu.tw>
2021-06-09 21:10:41 +0800

    Modify the code according to SonarQube

--- app/src/main/java/main/Main.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import java.text.SimpleDateFormat;
-
-public class Main {
-
-	public static void main(String[] args) {
-	}
-}
--- app/src/main/java/main/SwimmingPool.java
+++ app/src/main/java/main/SwimmingPool.java
@@ -1,3 +1,4 @@
+package main;
 
 public class SwimmingPool {
 	public int getPrice(boolean isWeekend, int age, int time, boolean isGroup, boolean isMember) {
